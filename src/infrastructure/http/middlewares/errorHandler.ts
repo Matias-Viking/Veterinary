288db5dd94a4c95405212031b23285fdda1d5e5d
Matias-Viking <matiasctau@gmail.com>
2025-12-17 18:21:08 -0300

    mas correcciones

--- src/infrastructure/http/middlewares/errorHandler.ts
+++ src/infrastructure/http/middlewares/errorHandler.ts
@@ -1,12 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import correlator from "express-correlation-id";
-import { ErrorMapper } from "../http/errors/ErrorMapper";
+import { ErrorMapper } from "../errors/ErrorMapper";
+
 
 
 export function errorHandler(error:unknown,_req: Request,res:Response,_next:NextFunction): void{
 
     const xCorrelator=correlator.getId();
-    const httpError= ErrorMapper.toHttpError(error,xCorrelator);
-    
-    
+    const httpError=ErrorMapper.toHttpError(error,xCorrelator);
 }
